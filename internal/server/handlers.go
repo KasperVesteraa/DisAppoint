57dcd6b98d283d04dd793e086aa9a90c2efde8b3
KasperVesteraa <kv19@student.aau.dk>
2024-04-13 13:30:25 +0200

    feat: added update user to CRUD

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -13,27 +13,10 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to DisAppoint!\n")
 }
 
-// func CreateUserHandler(db *sql.DB) http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		var user api.User
-// 		if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
-// 			http.Error(w, err.Error(), http.StatusBadRequest)
-// 			return
-// 		}
-// 		_, err := db.Exec("INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4)", user.Id, user.Name, user.Email, user.Password)
-// 		if err != nil {
-// 			http.Error(w, err.Error(), http.StatusInternalServerError)
-// 			return
-// 		}
-// 		w.WriteHeader(http.StatusCreated)
-// 		fmt.Fprintf(w, "User created successfully!\n")
-// 	}
-// }
-
 func UserHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
-		case http.MethodPost:
+		case http.MethodPost: // Create
 			var user api.User
 			if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
 				http.Error(w, err.Error(), http.StatusBadRequest)
@@ -47,7 +30,7 @@ func UserHandler(db *sql.DB) http.HandlerFunc {
 			}
 			w.WriteHeader(http.StatusCreated)
 			fmt.Fprintf(w, "User created successfully!\n")
-		case http.MethodDelete:
+		case http.MethodDelete: // Delete
 			var user api.User
 			if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
 				http.Error(w, err.Error(), http.StatusBadRequest)
@@ -61,6 +44,20 @@ func UserHandler(db *sql.DB) http.HandlerFunc {
 			}
 			w.WriteHeader(http.StatusOK)
 			fmt.Fprintf(w, "User deleted successfully!\n")
+		case http.MethodPut: // Update
+			var user api.User
+			if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			defer r.Body.Close()
+			_, err := db.Exec("UPDATE users SET name = $1, password = $2 WHERE email = $3", user.Name, user.Password, user.Email)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintf(w, "User updated succesfully!\n")
 		default:
 			http.Error(w, "Unsupported HTTP method", http.StatusMethodNotAllowed)
 		}
